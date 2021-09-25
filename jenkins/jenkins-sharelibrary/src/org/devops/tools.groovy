b5afc3946bc0ae4f1204e6e1fe8e2da77916c2f1
test <test@163.com>
2021-09-25 23:34:03 +0800

    jenkins sharelibrary 共享库测试

---
+++ jenkins/jenkins-sharelibrary/src/org/devops/tools.groovy
@@ -0,0 +1,12 @@
+package org.devops
+
+//¸ñÊ½»¯Êä³ö
+def PrintMes(value,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
+              'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
+}
