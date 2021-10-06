311e2ae20323a08bb23ed90cf82ef670e2d568b4
test <test@163.com>
2021-10-06 12:15:21 +0800

    jenkins pipeline+gitlab构建maven项目：构建信息邮件通知

---
+++ jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/toemail.groovy
@@ -0,0 +1,35 @@
+package org.devops
+
+//定义邮件内容
+def Email(status,emailUser){
+    emailext body: """
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <meta charset="UTF-8">
+            </head>
+            <body leftmargin="8" marginwidth="0" topmargin="8" marginheight="4" offset="0">
+                <img src="http://gitlab.mieken.cn/uploads/-/system/user/avatar/1/avatar.png">
+                <table width="95%" cellpadding="0" cellspacing="0" style="font-size: 11pt; font-family: Tahoma, Arial, Helvetica, sans-serif">
+                    <tr>
+                        <td><br />
+                            <b><font color="#0B610B">构建信息</font></b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <ul>
+                                <li>项目名称：${JOB_NAME}</li>
+                                <li>构建编号：${BUILD_ID}</li>
+                                <li>构建状态: ${status} </li>
+                                <li>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></li>
+                                <li>构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+            </html>  """,
+            subject: "Jenkins-${JOB_NAME}项目构建信息 ",
+            to: emailUser
+}
