11e17e2a27fa0dd4395e993a55e59e2ff5837976
test <test@163.com>
2021-10-09 23:21:33 +0800

    sharelibrary共享库sonarqube.groovy添加withSonarQubeEnv扫描，使得jenkinsfile直接跳转sonarServer

--- jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/sonarqube.groovy
+++ jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/sonarqube.groovy
@@ -2,24 +2,22 @@ package org.devops
 
 //scanner
 def SonarScan(projectName,projectDsec,projectPath,sonarScanHome){
-    def sonarServer = "http://sonar.mieken.cn"
-    def sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
-    sonarDate = sonarDate - "\n"
+    withSonarQubeEnv("SonarQube"){
+        def sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
+        sonarDate = sonarDate - "\n"
 
-    sh """
-        ${sonarScanHome}/sonar-scanner -Dsonar.host.url=${sonarServer} \
-        -Dsonar.projectKey=${projectName} \
-        -Dsonar.projectName=${projectName} \
-        -Dsonar.projectVersion=${sonarDate} \
-        -Dsonar.login=admin \
-        -Dsonar.password=Sonar@123 \
-        -Dsonar.ws.timeout=30 \
-        -Dsonar.projectDescription=${projectDsec} \
-        -Dsonar.links.homepage=http://www.baidu.com \
-        -Dsonar.sources=${projectPath} \
-        -Dsonar.sourceEncoding=UTF-8 \
-        -Dsonar.java.binaries=target/classes \
-        -Dsonar.java.test.binaries=target/test-classes \
-        -Dsonar.java.surefire.report=target/surefire-reports
-    """
+        sh """
+            ${sonarScanHome}/sonar-scanner -Dsonar.projectKey=${projectName} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectVersion=${sonarDate} \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDsec} \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=${projectPath} \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports
+        """
+    }
 }
