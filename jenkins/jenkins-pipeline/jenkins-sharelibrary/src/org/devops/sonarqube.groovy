558cc0b1ac3eacd42acaef66f2f083edfadcf57b
test <test@163.com>
2021-10-13 14:12:19 +0800

    Sonar配置扫描结果与Commit关联

--- jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/sonarqube.groovy
+++ jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/sonarqube.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 //scanner
-def SonarScan(sonarServer,projectName,projectDsec,projectPath){
+def SonarScan(sonarServer,projectName,projectDsec,projectPath,runOpts='',projectId='',commitSha='',branchName=''){
 
     def sonarScanHome = tool "SONARSCANNER"
 
@@ -12,19 +12,25 @@ def SonarScan(sonarServer,projectName,projectDsec,projectPath){
         def sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
 
-        sh """
-            ${sonarScanHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-            -Dsonar.projectName=${projectName} \
-            -Dsonar.projectVersion=${sonarDate} \
-            -Dsonar.ws.timeout=30 \
-            -Dsonar.projectDescription=${projectDsec} \
-            -Dsonar.links.homepage=http://www.baidu.com \
-            -Dsonar.sources=${projectPath} \
-            -Dsonar.sourceEncoding=UTF-8 \
-            -Dsonar.java.binaries=target/classes \
-            -Dsonar.java.test.binaries=target/test-classes \
-            -Dsonar.java.surefire.report=target/surefire-reports
-        """
+        if ("${runOpts}" == "GitlabPush"){
+            sh """
+                ${sonarScanHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} -Dsonar.projectName=${projectName} \
+                -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 -Dsonar.projectDescription=${projectDsec} \
+                -Dsonar.links.homepage=http://www.baidu.com -Dsonar.sources=${projectPath} \
+                -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
+                -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports \
+                -Dsonar.java.analysis.mode=issues -Dsonar.gitlab.project_id=${projectId} \
+                -Dsonar.gitlab.commit_sha=${commitSha} -Dsonar.gitlab.ref_name=${branchName}
+            """
+        } else {
+             sh """
+                ${sonarScanHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} \
+                -Dsonar.ws.timeout=30 -Dsonar.projectDescription=${projectDsec} -Dsonar.links.homepage=http://www.baidu.com \
+                -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
+                -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports
+            """
+        }
     }
     // 质量阈计算结果
     //def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
