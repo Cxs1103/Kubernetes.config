870ef8fd082b82c707d10d09417315725aaa07f6
test <test@163.com>
2021-10-17 14:51:09 +0800

    Sharelibrary 封装Maven shell命令 和 Nexus artifact 插件上传制品到Nexus

---
+++ jenkins/jenkins-pipeline/jenkins-sharelibrary/src/org/devops/nexus.groovy
@@ -0,0 +1,60 @@
+package org.devops
+
+// 获取POM文件中的坐标
+def GetGav(){
+    def jarName = sh returnStdout: true, script: "cd target;ls *.jar"
+    env.jarName = jarName - "\n"
+
+    def pom = readMavenPom file: 'pom.xml'
+    env.pomVersion = "${pom.version}"
+    env.pomArtifact = "${pom.artifactId}"
+    env.pomPackaging = "${pom.packaging}"
+    env.pomGroupId = "${pom.groupId}"
+
+    println("${pomGroupId}-${pomArtifact}-${pomVersion}-${pomPackaging}")
+
+    return ["${pomGroupId}","${pomArtifact}","${pomVersion}","${pomPackaging}"]
+}
+
+// 集成Maven命令上传制品
+def MavenUpload(){
+def mvnHome = tool "M2"
+sh  """
+    cd target/
+    ${mvnHome}/bin/mvn deploy:deploy-file -Dmaven.test.skip=true \
+                                          -Dfile=${jarName} -DgroupId=${pomGroupId} \
+                                          -DartifactId=${pomArtifact} -Dversion=${pomVersion} \
+                                          -Dpackaging=${pomPackaging} -DrepositoryId=maven-hostd \
+                                          -Durl=http://nexus.mieken.cn/repository/maven-hostd
+    """
+}
+
+
+// 使用 nexus artifact uploader 上传制品
+def NexusUpload(){
+def repoName = "maven-hostd"
+def filePath = "target/${jarName}"
+nexusArtifactUploader artifacts: [[artifactId: "${pomArtifact}",
+                                    classifier: '',
+                                    file: "${filePath}",
+                                    type: "${pomPackaging}"]],
+                        credentialsId: 'Nexus-auth',
+                        groupId: "${pomGroupId}",
+                        nexusUrl: 'nexus.mieken.cn',
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        repository: "${repoName}",
+                        version: "${pomVersion}"
+}
+
+def main(uploadType){
+    // 读取pom
+    GetGav()
+
+    // 判断上传方式
+    if ("${uploadType}" == "maven"){
+        MavenUpload()
+    } else if("${uploadType}" == "nexus"){
+        NexusUpload()
+    }
+}
