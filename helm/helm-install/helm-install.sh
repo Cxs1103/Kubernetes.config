a17c661771b3f69a938e674e2f1db734f1addeb9
test <test@163.com>
2022-09-26 16:26:09 +0800

    helm安裝腳本

---
+++ helm/helm-install/helm-install.sh
@@ -0,0 +1,15 @@
+# github下载地址
+
+https://github.com/helm/helm/releases/tag/v3.6.1
+
+# 官网下载地址
+
+wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
+
+# 解压Helm到/usr/bin目录：
+
+tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
+
+cd linux-amd64/
+
+cp helm /usr/bin/
